6d01fb5ff25641108859a9862b64c17ffee20ffa
Adrian Jenkins <adrian.jenkins99@gmail.com>
2021-04-03 23:01:39 -0400

    Initial Commit

---
+++ Group_Study_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group_Study_App
+//
+//  Created by Adrian Jenkins on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
