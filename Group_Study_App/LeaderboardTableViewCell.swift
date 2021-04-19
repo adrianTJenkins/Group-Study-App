591abcac741dbfc1a90bac155c3e51f5a6702236
Adrian Jenkins <adrian.jenkins99@gmail.com>
2021-04-18 23:36:14 -0400

    defined TableViewCell classes

---
+++ Group_Study_App/LeaderboardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LeaderboardTableViewCell.swift
+//  Group_Study_App
+//
+//  Created by Adrian Jenkins on 4/18/21.
+//
+
+import UIKit
+
+class LeaderboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Group_Study_App/QuizListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuizListTableViewCell.swift
+//  Group_Study_App
+//
+//  Created by Adrian Jenkins on 4/18/21.
+//
+
+import UIKit
+
+class QuizListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
